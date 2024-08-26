fb03eababa2bb4c1ae650b790c823488defc8787
Nishan <35008825+ametanishant@users.noreply.github.com>
2024-08-26 13:11:31 +0530

    Update test.groovy , removed body

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,18 +1,12 @@
-def call(body) {
+def call() {
         
     // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    env.LC_ALL = "C.UTF-8"
-    env.LANG = "C.UTF-8"
 
     def jobName = env.JOB_NAME
     def buildURL = env.BUILD_URL
     def buildId = env.BUILD_ID
 
-// Print the jobName
+    // Print the jobName
     echo "The Job Name is: ${jobName}"
 
 }
