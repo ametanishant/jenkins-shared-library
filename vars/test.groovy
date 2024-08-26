16665eb7f1c19290b83bfef6595eb90e4d33371a
Nishan <35008825+ametanishant@users.noreply.github.com>
2024-08-26 13:20:40 +0530

    Added Service Type

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,10 +1,17 @@
 def call(body) {
         
     // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()    
+    def test = config.service_type
 
-    def job_Name = env.JOB_NAME
+    def jobName = env.JOB_NAME
     def buildURL = env.BUILD_URL
     def buildId = env.BUILD_ID
+
+
 
     // Print the jobName
     echo "The Job Name is: ${jobName}"
