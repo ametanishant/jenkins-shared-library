2f079a04102d2fb54f662c4044b6ed481f6a017e
nameta <nameta@cisco.com>
2024-09-25 14:21:03 +0530

    "Add BuildConfig"

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,6 @@
+package com.nishant
+
+import com.nishant.BuildConfig // Import the BuildConfig class
 
 def call(body) {
         
@@ -11,10 +14,12 @@ def call(body) {
     def buildURL = env.BUILD_URL
     def buildId = env.BUILD_ID
     def SerName = config.service_type
+    def patchsetJobName = BuildConfig.PATCHSET_JOB
         
     echo "The Job Name  is: ${jobName}"
     echo "The buildURL is: ${buildURL}"
     echo "The Service Name is : ${SerName}"
+    echo "The Patchset Job Name is: ${patchsetJobName}"
     
    
     
