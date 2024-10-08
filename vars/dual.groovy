16997ccda10df47ec4b6d9818574cedb0c121207
Nishan <35008825+ametanishant@users.noreply.github.com>
2024-10-08 11:44:43 +0530

    Create dual.groovy

---
+++ vars/dual.groovy
@@ -0,0 +1,9 @@
+def call1(body) {
+    println "call1 was called with body: ${body}"
+    // Add your logic here
+}
+
+def call2(body) {
+    println "call2 was called with body: ${body}"
+    // Add your logic here
+}
