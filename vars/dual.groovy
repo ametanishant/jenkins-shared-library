35b2533cc4f52ef5bbe88ae0d2802102f4026495
Nishan <35008825+ametanishant@users.noreply.github.com>
2024-10-08 12:04:59 +0530

    Update dual.groovy

--- vars/dual.groovy
+++ vars/dual.groovy
@@ -4,12 +4,12 @@ def jobOne() {
     echo 'Executing Job One...'
     // Add your logic for Job One here
     // For example, running a shell command
-    sh 'echo Hello from Job One'
+    echo 'echo Hello from Job One'
 }
 
 def jobTwo() {
     echo 'Executing Job Two...'
     // Add your logic for Job Two here
     // For example, building a Docker image
-    sh 'echo Building Docker Image for Job Two'
+    echo 'echo Building Docker Image for Job Two'
 }
