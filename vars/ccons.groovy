ecd7de2d200771c32640c52b653339249ecf2b61
Nishan <35008825+ametanishant@users.noreply.github.com>
2024-10-10 12:40:29 +0530

    Create ccons.groovy

---
+++ vars/ccons.groovy
@@ -0,0 +1,24 @@
+def checkc(body) {
+    // Evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    env.LC_ALL = "C.UTF-8"
+    env.LANG = "C.UTF-8"
+
+    // 'buildName' is passed from the pipeline 
+    def buildName = config.buildName
+    def nodeName = config.nodeName
+
+
+    def branchName
+    if (buildName == 'B' || buildName == 'Bu') {
+        branchName = 'ari'
+    } else if (['Confi', 'Craf', 'darwi', 'release'].contains(buildName)) {
+        branchName = 'vee'
+    }
+    println ${branchName}
+}
+    
