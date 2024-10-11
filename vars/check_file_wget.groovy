2c22e728e2e82144adcab6a1c6d8563f76ffbbe7
Nishan <35008825+ametanishant@users.noreply.github.com>
2024-10-11 17:03:38 +0530

    Update check_file_wget.groovy

--- vars/check_file_wget.groovy
+++ vars/check_file_wget.groovy
@@ -8,36 +8,39 @@ def call(body) {
     body.delegate = config
     body()
 
-    // Set environment variables
-    env.LC_ALL = "C.UTF-8"
-    env.LANG = "C.UTF-8"
+    // Ensure execution within a node
+    node {
+        // Set environment variables
+        env.LC_ALL = "C.UTF-8"
+        env.LANG = "C.UTF-8"
 
-    // 'buildName' is passed from the pipeline and will replace 'bighorn' in the file URL
-    def buildName = config.buildName 
+        // 'buildName' is passed from the pipeline and will replace 'bighorn' in the file URL
+        def buildName = config.buildName 
 
-    // Define the file URL to download the tar.xz file, where 'buildName' is the variable part
-    def fileUrl = "https://download.freedict.org/dictionaries/deu-eng/1.9-fd1/freedict-deu-eng-1.9-fd1.dictd.tar.xz"
-    def outputFile = "freedict-deu-eng-1.9-fd1.dictd.tar.xz"
+        // Define the file URL to download the tar.xz file, where 'buildName' is the variable part
+        def fileUrl = "https://download.freedict.org/dictionaries/deu-eng/1.9-fd1/freedict-deu-eng-1.9-fd1.dictd.tar.xz"
+        def outputFile = "freedict-deu-eng-1.9-fd1.dictd.tar.xz"
 
-    try {
-        echo "Entering Try Loop....."
-        // Download the tar.xz file using 'wget'
-        sh "export PATH=/opt/homebrew/bin:\$PATH && wget -O ${outputFile} ${fileUrl}"
+        try {
+            echo "Entering Try Loop....."
+            // Download the tar.xz file using 'wget'
+            sh "export PATH=/opt/homebrew/bin:\$PATH && wget -O ${outputFile} ${fileUrl}"
 
-        // Extract the tar.xz file
-        sh "tar -xvf ${outputFile}"
+            // Extract the tar.xz file
+            sh "tar -xvf ${outputFile}"
 
-        // If extraction is successful
-        echo "Created tar file properly"
-    } catch (Exception e) {
-        // If the extraction or download fails, trigger the Jenkins job using 'curl'
-        echo "Error in creating tar file, triggering Jenkins build..."
+            // If extraction is successful
+            echo "Created tar file properly"
+        } catch (Exception e) {
+            // If the extraction or download fails, trigger the Jenkins job using 'curl'
+            echo "Error in creating tar file, triggering Jenkins build..."
 
-        // Build the Jenkins URL dynamically based on 'jenkinsJobName'
-        def jenkinsJobName = config.jenkinsJobName ?: 'DefaultJob'
-        def jenkinsUrl = "http://localhost:8080/job/${jenkinsJobName}/build"
+            // Build the Jenkins URL dynamically based on 'jenkinsJobName'
+            def jenkinsJobName = config.jenkinsJobName ?: 'DefaultJob'
+            def jenkinsUrl = "http://localhost:8080/job/${jenkinsJobName}/build"
 
-        // Trigger the Jenkins build using 'curl'
-        sh "curl -X POST ${jenkinsUrl}"
+            // Trigger the Jenkins build using 'curl'
+            sh "curl -X POST ${jenkinsUrl}"
+        }
     }
 }
