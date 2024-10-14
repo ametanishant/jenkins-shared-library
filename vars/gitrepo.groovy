640dc69824ef91ff61fb90c3a0b48ba74714a820
Nishan <35008825+ametanishant@users.noreply.github.com>
2024-10-14 17:58:07 +0530

    Create gitrepo.groovy

---
+++ vars/gitrepo.groovy
@@ -0,0 +1,17 @@
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    env.LC_ALL = "C.UTF-8"
+    env.LANG = "C.UTF-8"
+    
+    // Initialize build details and configuration
+  
+
+    // Extract variables from the config and environment
+    def git_repo = config.GIT_REPO_URL  
+    
+  
+
+}
